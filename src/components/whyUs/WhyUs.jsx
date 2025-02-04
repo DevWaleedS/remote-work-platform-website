ef12025ee4b1daebf7339fc2eeaba501acdadef2
DevWaleedS <eng.waleed.salah@outlook.com>
2025-02-04 17:53:12 +0200

    add Vision2030 icon to WhyUs component; enhance styling and positioning for better visibility

--- src/components/whyUs/WhyUs.jsx
+++ src/components/whyUs/WhyUs.jsx
@@ -7,6 +7,7 @@ import "./WhyUs.css";
 
 import { useNavigate } from "react-router-dom";
 import { HowToStart } from "../../assets/Img";
+import { Vision2030 } from "../../assets/Icons";
 
 const WhyUs = () => {
 	const navigate = useNavigate();
@@ -20,7 +21,7 @@ const WhyUs = () => {
 		<div className='why-us-box '>
 			<div className='why-us-body d-flex flex-md-row flex-column-reverse justify-content-center align-items-center'>
 				<div
-					className='why-us-img d-md-inline-block d-none'
+					className='position-relative why-us-img d-md-inline-block d-none '
 					data-aos-once='true'
 					data-aos='fade-down'
 					data-aos-delay='400'>
@@ -30,6 +31,9 @@ const WhyUs = () => {
 						loading='lazy'
 						style={{ mixBlendMode: "multiply" }}
 					/>
+					<div className='position-absolute vision2030-icon'>
+						<Vision2030 />
+					</div>
 				</div>
 
 				<section className='why-us-text'>
@@ -42,7 +46,7 @@ const WhyUs = () => {
 						إدارة فريقك بكفاءة من أي مكان! نوفر لك أدوات ذكية لتوظيف الموظفين،
 						متابعة الأداء،
 						<div
-							className='why-us-img d-md-none d-inline-block'
+							className='position-relative why-us-img d-md-none d-inline-block'
 							data-aos-once='true'
 							data-aos='fade-down'
 							data-aos-delay='400'>
@@ -51,6 +55,9 @@ const WhyUs = () => {
 								alt='ليش تختار منصة المعرفة للعمل عن بُعد'
 								loading='lazy'
 							/>
+							<div className='position-absolute vision2030-icon'>
+								<Vision2030 />
+							</div>
 						</div>
 						تنظيم المهام، وإدارة الرواتب بسهولة. ✨ بالإضافة إلى ذلك، تعزز
 						منصتنا التواصل بين الفريق، مما يضمن إنتاجية أعلى وانسيابية في العمل.
